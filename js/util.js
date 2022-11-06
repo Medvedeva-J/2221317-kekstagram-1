27683b3b1db7d4a3df5d097896670cbd8377e5c6
Medvedeva-J <62373163+Medvedeva-J@users.noreply.github.com>
2022-11-06 14:17:27 +0500

    main.js разделен на отдельные модули_4

--- js/util.js
+++ js/util.js
@@ -1,13 +1,13 @@
 import {COMMENTS, NAMES, commentsIdList} from './data.js';
 
 function getRandomNumber(from, to) {
+  let result;
   if (from < 0 || to < 0) {
     return 'Диапазон может быть только положительным';
   }
-  let result;
-  if (from > to) {                                                    //При значении "от", большим, чем значение "до", функция переворачивает данный отрезок и всё равно возвращает псевдорандомное числов указанном диапазоне.
-      result = Math.floor(Math.random() * (from - to + 1) + to);     //При равных введённых значениях "от" и "до" функция вернёт это значение.
-  } else {
+  if (from > to) {
+    result = Math.floor(Math.random() * (from - to + 1) + to);       //При значении "от", большим, чем значение "до", функция переворачивает данный отрезок и всё равно возвращает псевдорандомное числов указанном диапазоне.
+  } else {                                                                     //При равных введённых значениях "от" и "до" функция вернёт это значение.
     result = Math.floor(Math.random() * (to - from + 1) + from);
   }
   return result;
@@ -19,25 +19,25 @@ function checkStringLength(line, maxLength) {
 //вот отсюда начинаются новые методы
 
 function createPhoto(id) {
-  let newPhoto = {
-      id: id,
-      url: `photos/${id}.jpg`,
-      description: 'Невероятно красивая картинка',
-      likes: getRandomNumber(15, 200),
-      comments: Array.from({length: getRandomNumber(1, 3)}, createComment)
+  const newPhoto = {
+    id: id,
+    url: `photos/${id}.jpg`,
+    description: 'Невероятно красивая картинка',
+    likes: getRandomNumber(15, 200),
+    comments: Array.from({length: getRandomNumber(1, 3)}, createComment)
   };
   return newPhoto;
 }
 
 function createComment() {
-  let newComment = {
-      id: commentsIdList.length,
-      avatar: `img/avatar-${getRandomNumber(1, 6)}.svg`,
-      message: COMMENTS[getRandomNumber(0, COMMENTS.length - 1)],
-      name: NAMES[getRandomNumber(0, NAMES.length - 1)]
-  }
+  const newComment = {
+    id: commentsIdList.length,
+    avatar: `img/avatar-${getRandomNumber(1, 6)}.svg`,
+    message: COMMENTS[getRandomNumber(0, COMMENTS.length - 1)],
+    name: NAMES[getRandomNumber(0, NAMES.length - 1)]
+  };
   commentsIdList.push(newComment.id);
   return newComment;
 }
 
-export {createPhoto, createComment, getRandomNumber, checkStringLength};
+export {getRandomNumber, checkStringLength, createPhoto, createComment};
